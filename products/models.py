3c0330c28b62becd21fcc2a4750c09be7e6e0400
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-09-12 11:39:04 +0000

    images added to products

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
