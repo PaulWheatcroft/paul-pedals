aab5b7e28ddc4d07985b3f64b3504614d5f20d67
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-09-12 10:57:15 +0000

    Products are returning from the database

--- products/models.py
+++ products/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField()
+
+    def __str__(self):
+        return self.title
--- products/views.py
+++ products/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 
-# Create your views here.
-def products_view(request, *args, **kwargs):
-    return render(request, "products.html", {})
+def products_view(request):
+    from_database = Product.objects.all()
+
+    my_context = {
+        "the_text": "these are words",
+        'the_number': 123,
+        'the_boolean': True,
+        'from_database': from_database,
+    }
+
+    # products = Product.objects.all()
+
+    return render(request, "products.html", my_context)
