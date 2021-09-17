66c0fe94992e37d07f6e1cb9c55350e9ae3b7775
PaulWheatcroft <paul_wheatcroft@hotmail.com>
2021-09-17 18:08:11 +0000

    Added remove item from bag

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('amend/<item_id>/', views.amend_to_bag, name='amend_to_bag'),
+    path('remove/<item_id>/', views.remove_bag_item, name='remove_bag_item'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -22,5 +22,38 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print('add_to_bag')
     print(request.session['bag'])
+    return redirect(redirect_url)
+
+
+def amend_to_bag(request, item_id):
+    """ View to add items to a bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag')
+
+    bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print('amend_to_bag')
+    print(request.session['bag'])
+    return redirect(redirect_url)
+
+
+def remove_bag_item(request, item_id):
+    """ View to add items to a bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+    item_number = item_id
+
+    del bag[item_number]
+
+    request.session['bag'] = bag
+
+    print('remove_bag_item')
+    print(bag)
+    print(item_number)
     return redirect(redirect_url)
